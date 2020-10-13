0b372f947f15dbd13e8cd46bcaa886758076685d
Oleh Lavryk <oleh.lavrik@gmail.com>
2020-10-13 19:50:48 +0300

    Create White header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import c from './../../App.module.scss';
 import { Link } from 'react-router-dom';
 import s from './Header.module.scss'
 import { Icon } from '../Icons/Icon';
 import { routes } from '../../scenes/routes';
+import './../../App.css';
 
 export const Header = () => {
 
   return(
     <header className={s.header}>
-      <div className={c.container}>
+      <div className="container">
         <div className={s.left}>
-          <Icon name='logo' />
+          <Link to={routes.home}>
+            <Icon name='logo' />
+          </Link>
         </div>
         <div className={s.right}>
-          <Link to={routes.login}>Login</Link>
+          <Link to={routes.home} className={s.primary_link}>Sell</Link>
+          <Link to={routes.login} className={s.outline_link}>Login</Link>
+          <Link to={routes.home}>
+            <Icon name="like" />
+          </Link>
         </div>
       </div>
     </header>
--- src/scenes/Auth/Auth.js
+++ src/scenes/Auth/Auth.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import s from './Auth.module.scss';
 import { Header } from '../../components/Header/Header';
 import { Switch, Route } from 'react-router-dom';
 import { routes } from './../routes';
