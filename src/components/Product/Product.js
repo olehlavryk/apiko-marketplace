7f85376ea4ee6f61f0c0e5387580c3414935f89d
Oleh Lavryk <oleh.lavrik@gmail.com>
2020-10-19 12:03:41 +0300

    Add check for boken product images at Home scene

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -5,7 +5,12 @@ import { Icon } from '../Icons/Icon';
 
 export const Product = (props) => {
   const { item } = props;
-  console.log(item);
+  const imagePlaceHolder = 'https://via.placeholder.com/500x500';
+
+  function addDefaultSrc(ev) {
+    ev.target.src = imagePlaceHolder;
+  }
+
   return (
     <div className={s.product}>
       <Link to="#">
@@ -14,17 +19,21 @@ export const Product = (props) => {
             src={item.photos[0]}
             alt={item.title}
             className={s.product_preview}
+            onError={addDefaultSrc}
           />
         ) : (
           <img
-            src="https://via.placeholder.com/500x500"
+            src={imagePlaceHolder}
             alt={item.title}
             className={s.product_preview}
           />
         )}
       </Link>
       <div className={s.product_info}>
-        <Link to="#" className={s.product_title}>{item.title.charAt(0).toUpperCase() + item.title.slice(1)}</Link>
+        <Link to="#" className={s.product_title}>
+          {item.title.charAt(0).toUpperCase()
+          + item.title.slice(1)}
+        </Link>
         <div className={s.product_price}>${item.price}</div>
         <Link to="#" className={s.product_like_wrap}>
           {item.save ? (
