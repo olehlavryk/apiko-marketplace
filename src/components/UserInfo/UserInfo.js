e93e3e1f6d637b6002b85ad8e01d6df9939f243d
Oleh Lavryk <oleh.lavrik@gmail.com>
2020-10-23 14:29:28 +0300

    Add fix to UserInfo picture

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './UserInfo.module.scss';
 import { Icon } from '../Icons/Icon';
+import { getImagePlaceHolderPath, setImagePlaceHolder } from '../../stores/utils';
 
 export const UserInfo = ({ product }) => {
   console.log(product);
@@ -11,7 +12,18 @@ export const UserInfo = ({ product }) => {
       <div className={s.seller_info}>
         <div className={s.seller_avatar}>
           {/* todo if avatar is empty set placeholder */}
-          <img src={avatar} alt={fullName} />
+          {avatar ? (
+            <img
+              src={avatar}
+              alt={fullName}
+              onError={(e) => setImagePlaceHolder(e, '40x40')}
+            />
+          ) : (
+            <img
+              src={getImagePlaceHolderPath('40x40')}
+              alt={fullName}
+            />
+          )}
         </div>
         <div className={s.seller_name}>{fullName}</div>
         <div className={s.seller_location}>{location}</div>
