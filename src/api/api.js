9f51485fb8697abe3c582980b9316704490ee3ec
Oleh Lavryk <oleh.lavrik@gmail.com>
2020-11-08 12:49:35 +0200

    Change base url to axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://olehlavryk.github.io/';
+axios.defaults.baseURL =
+  'https://olehlavryk.github.io/apiko-marketplace/';
 
 export const Auth = {
   _token: null,
