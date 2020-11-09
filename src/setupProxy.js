fdab543eba64b9c4dfe4df0490c3cfaf182f3bf0
Oleh Lavryk <oleh.lavrik@gmail.com>
2020-11-09 11:53:45 +0200

    Small changes for setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,11 +12,3 @@ module.exports = (app) => {
     }),
   );
 };
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log(
-    'Express server listening on port %d in %s mode',
-    this.address().port,
-    app.settings.env,
-  );
-});
